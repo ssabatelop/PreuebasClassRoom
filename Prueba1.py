8caa61a8084813c774988abd1177d69f4dc45ffe
ssabatelop <ssabatelop@educacion.navarra.es>
2023-10-31 08:21:15 +0100

    Update Prueba1.py

--- Prueba1.py
+++ Prueba1.py
@@ -1,4 +1,21 @@
-x = range(3+1)
+x = "En un lugar de la mancha de cullo nombre no quiero acordarme"
 print(x)
-for imp in x:
-    print(imp)
+print(len(x))
+print(type(x))
+print(list(x))
+print(type(list(x)))
+print(len(x))
+
+print(x.count('l'))
+
+# v = ([1, 2, 3], [3, 2, 1])
+# print(v[0])
+# v[0] = [3, 2, 1]
+# print(v)
+# v[1][0]=1
+# print(v[1][0])
+# print(v)
+
+x = [11, 1, 1, 1, 1]
+#x = str(x)
+print(x[0] + 2)
